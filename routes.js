bc3e55a67687cc1aefd6eb2b9c85582ec013a3c8
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-07-16 14:27:11 +0100

    Began work on ts, typeOrm conversion backend improvements

--- routes.js
+++
@@ -1,6 +0,0 @@
-exports.routes = [
-    { path: '/home', location: './endpoints/landing', func: 'init' },
-    { path: '/chatgen', location: './endpoints/chatgen', func: 'sessionGen' },
-    { path: '/authtoken', location: './endpoints/authToken', func: 'checkToken' }
-]
-
